ed8d8f27df9593bcee6b2bbb8c2abe1e5ff68e94
kevin344k <105673385+kevin344k@users.noreply.github.com>
2023-03-23 23:43:58 +0000

    dsfdf

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express=require("express")
+const {createServer}=require("http")
+const realTimeServer=require("./realTimeServer")
+const path=require("path")
+
+const app=express()
+const httpServer=createServer(app)
+
+//setting
+app.set("port",process.env.PORT||3000)
+app.set("views",path.join(__dirname,"views"))
+
+//routes
+
+app.use(require("./routes"))
+
+//public
+app.use(express.static(path.join(__dirname,"public")))
+
+//levanto el servidor
+httpServer.listen(app.get("port"),()=>{
+  console.log("el servidor esta corriendo en el puerto",app.get("port"))
+
+  console.log(path.join(__dirname,"public"))
+  
+})
+//llamo al servidor de socket.io
+
+realTimeServer(httpServer)
---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+const express=require("express")
+const router=express.Router()
+const path=require("path")
+
+const views=path.join(__dirname,"/../views")
+
+router.get("/",(req,res)=>{
+  res.sendFile(views+"/index.html")
+})
+
+router.get("/register",(req,res)=>{
+  res.sendFile(views+"/register.html")
+})
+
+
+module.exports=router
